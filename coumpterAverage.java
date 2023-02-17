340088e1c60dab4eac96327f20b2e3606d08c115
Shivam Thapa <113630018+Shivam453T3@users.noreply.github.com>
2023-02-17 13:17:58 +0530

    Add files via upload

---
+++ coumpterAverage.java
@@ -0,0 +1,18 @@
+public class coumpterAverage
+{
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+       int a, b, c;
+       int sum, avg ;
+       a =6;
+       b =4;
+       c =8;
+       sum = a + b + c;
+       avg = sum / 3;
+       System.out.println("The value of the positive integes");
+       System.out.println("First integer: " a + " , second integer :" + b +", Third integer :" + c);
+       System.out.println("There average is:" + average + ".");
+    }
+}
